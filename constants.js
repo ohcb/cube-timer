eb016368cba01e400db2d9a8599ba4fe00e81962
ohcb <dohyunk0609@gmail.com>
2026-06-17 23:10:49 +0900

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,18 @@
+const WCA_EVENTS = [
+  ['333','3x3'],
+  ['222','2x2'],
+  ['444','4x4'],
+  ['555','5x5'],
+  ['666','6x6'],
+  ['777','7x7'],
+  ['333oh','3x3 OH'],
+  ['333bf','3x3 BLD'],
+  ['minx','Megaminx'],
+  ['pyram','Pyraminx'],
+  ['skewb','Skewb'],
+  ['sq1','Square-1'],
+  ['clock','Clock']
+];
+
+const STORAGE_KEY = 'cube-timer-timer-first-state-v2';
+const READY_DELAY_MS = 700;
